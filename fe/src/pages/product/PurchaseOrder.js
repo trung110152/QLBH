caa6fee138886e9fa039e37ff3fceac22130a5a5
namngo93 <ngonam199380@gmail.com>
2023-03-03 00:20:56 +0700

    sua form managerOrder & sua sql orderSevice

--- fe/src/pages/product/PurchaseOrder.js
+++ fe/src/pages/product/PurchaseOrder.js
@@ -31,49 +31,77 @@ export default function PurchaseOrder() {
             {orders.length === 0 && <><p style={{textAlignLast:"center", fontSize:100, height:300, marginTop:200}}>No order </p></>}
             {orders !== 'Can not find by id order' && orders.map((order, index) => (
                 <>
-                    <div className="row" id="table1">
-                        <div className="col-12">
-                                <p> Order ID: {order.id}</p>
-                                <p>Total Price: {order.totalPoint}</p>
-                                <p>Time: {order.time}</p>
-                                <p>Status: {order.status}</p>
+                    <div className="row">
+                        <div className="col-12" >
+                            <table style={{textAlign: "center" }}>
+                                <thead>
+                                <tr>
+                                    <th>Order ID</th>
+                                    <th>Total Price</th>
+                                    <th>Time</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                                <tbody>
+                                <tr>
+                                    <td>{order.id}</td>
+                                    <td>{order.totalPoint}</td>
+                                    <td>{order.time}</td>
+                                    <td>{order.status}</td>
+                                </tr>
 
-                            <button className="btn btn-outline-secondary" onClick={() => {
-                                dispatch(showCart(order.id))
-                                setI(index)
-                            }}>Detail
-                            </button>
+                                 </tbody>
+                                        <button className="btn btn-outline-secondary" onClick={() => {
+                                            dispatch(showCart(order.id))
+                                            setI(index)
+                                        }}>Detail
+                                        </button>
+                            </table>
                         </div>
                     </div>
 
-                        <table>
                             {index === i && carts !== 'Saved cart' &&
                                 <>
-                                    <thead>
-                                    <tr className='.bg-secondary'>
-                                        <th>Image</th>
-                                        <th>Name</th>
-                                        <th>Price</th>
-                                        <th>Quantity</th>
-                                        <th>Total</th>
-                                        <th>Description</th>
-                                    </tr>
-                                    </thead>
-                                    {carts.map(item => (
-                            <tbody>
-                            <tr>
-                                <td><img src={item.image} alt="" style={{width: 50}}/></td>
-                                <td>{item.name}</td>
-                                <td>{item.price}</td>
-                                <td>{item.quantity}</td>
-                                <td>{item.total}</td>
-                                <td>{item.description}</td>
-                            </tr>
-                            </tbody>
+                                    <div className="row" id="table1">
+                                        <div className="col-12">
+                                            <div className="shopping-cart section">
+                                                <div className="container">
+                                                    <div className="row">
+                                                        <div className="col-12">
+                                                            <table className="table shopping-summery">
+                                                                <thead>
+                                                                    <tr className='.bg-secondary'>
+                                                                        <th>Image</th>
+                                                                        <th>Name</th>
+                                                                        <th>Price</th>
+                                                                        <th>Quantity</th>
+                                                                        <th>Total</th>
+                                                                        <th>Description</th>
+                                                                    </tr>
+                                                                </thead>
 
-                                    ))}</>
+                                                                 {carts.map(item => (
+                                                                <tbody>
+                                                                <tr>
+                                                                    <td><img src={item.image} alt="" style={{width: 50}}/></td>
+                                                                    <td>{item.name}</td>
+                                                                    <td>{item.price}</td>
+                                                                    <td>{item.quantity}</td>
+                                                                    <td>{item.total}</td>
+                                                                    <td>{item.description}</td>
+                                                                </tr>
+                                                                </tbody>
+                                                                ))}
+                                                            </table>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
                                 }
-                        </table>
+
                     <hr/>
                 </>
 
